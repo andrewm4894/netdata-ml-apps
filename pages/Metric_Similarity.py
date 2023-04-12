690a44e1a1995bda1a4f8ddb1431547710a78791
Andrew Maguire <andrewm4894@gmail.com>
2023-04-12 12:45:22 +0100

    add comments

--- pages/Metric_Similarity.py
+++ pages/Metric_Similarity.py
@@ -22,7 +22,9 @@ charts_regex = st.sidebar.text_input('charts_regex', value='system|apps|users|se
 freq = st.sidebar.text_input('freq', value='15s')
 top_n = st.sidebar.number_input('top_n', value=10)
 
-if run: 
+if run:
+    
+    # get data from agent 
     df = get_data(
         hosts=[host], 
         after=after, 
@@ -31,19 +33,33 @@ if run:
         freq=freq
         )
     print(df.shape)
+    
+    # copy raw data for plotting later
     df_raw = df.copy()
+    
+    # normalize data for similarity calculation
     df = (df - df.min()) / (df.max() - df.min())
     df = df.fillna(0)
+    
+    # get metric distances
     df_dist = pd.DataFrame(
         data=zip(df.columns, cdist(df[[target]].fillna(0).transpose(), df.fillna(0).transpose(), 'cosine')[0]),
         columns=['metric', 'distance']
     )
+    
+    # rank based on similarity
     df_dist['rank'] = (1 - df_dist['distance']).rank(ascending=False)
+    
+    # sort based on rank
     df_dist = df_dist.sort_values('rank')
+    
+    # get top_n cols to plot
     plot_cols = df_dist.head(top_n)['metric'].values.tolist()
     
+    # plot top_n cols distances
     st.dataframe(df_dist.head(top_n))
     
+    # plot top_n most similar metrics
     for col in plot_cols:
         st.line_chart(df_raw[col])
 
